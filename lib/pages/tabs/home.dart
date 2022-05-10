527bfe9c8c458f3512557aa94ed532633de7ad59
zengchanghuan <zengchanghuan@gmail.com>
2022-05-10 19:36:30 +0800

    add titleWidget

--- lib/pages/tabs/home.dart
+++ lib/pages/tabs/home.dart
@@ -11,7 +11,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   //轮播图
   Widget _swiperWidget() {
     List<Map> imgList = [
@@ -22,27 +21,53 @@ class _HomePageState extends State<HomePage> {
 
     return Container(
       child: AspectRatio(
-        aspectRatio: 2/1,
+        aspectRatio: 2 / 1,
         child: Swiper(
             itemBuilder: (BuildContext context, int index) {
-              return new Image.network(
+              return Image.network(
                 imgList[index]["url"],
                 fit: BoxFit.fill,
               );
             },
             itemCount: imgList.length,
-            pagination: new SwiperPagination(),
+            pagination: SwiperPagination(),
             autoplay: true),
       ),
     );
   }
 
-    @override
-    Widget build(BuildContext context) {
-      return ListView(
-        children: [
-          _swiperWidget(),
-        ],
-      );
-    }
+  Widget _titleWidget(value) {
+    return Container(
+      height: 32.0,
+      margin: EdgeInsets.only(left: 10.0),
+      padding: EdgeInsets.only(left: 10.0),
+      decoration: BoxDecoration(
+          border: Border(
+              left: BorderSide(
+        color: Colors.red,
+        width: 10.0,
+      ))),
+      child: Text(
+        value,
+        style: TextStyle(
+          color: Colors.black54,
+        ),
+      ),
+    );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        _swiperWidget(),
+        SizedBox(height: 20.0),
+        _titleWidget('猜你喜欢'),
+        SizedBox(height: 20.0),
+        _titleWidget('热门推荐'),
+
+
+      ],
+    );
+  }
+}
