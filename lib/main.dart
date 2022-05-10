6a923f88140c134024230f6b0e0734192cef6c75
zengchanghuan <zengchanghuan@gmail.com>
2022-05-10 15:47:18 +0800

    添加统一路由

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'pages/tabs/tabs.dart';
+import 'routes/router.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,7 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Tabs(),
+      //
+        initialRoute: '/',
+        onGenerateRoute:onGenerateRoute,
     );
+
   }
 }
