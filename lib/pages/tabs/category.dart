f006459196dc84f94888531776feedb36b03f347
zengchanghuan <zengchanghuan@gmail.com>
2022-05-11 18:18:46 +0800

    导航listView

--- lib/pages/tabs/category.dart
+++ lib/pages/tabs/category.dart
@@ -10,6 +10,43 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    return Text('CategoryPage');
+    return Row(
+      children: [
+        Container(
+          width: 140,
+          height: double.infinity,
+          color: Colors.white54,
+          child: ListView.builder(
+            itemCount: 28,
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      height: 56,
+                      width: double.infinity,
+                      child: Text(
+                        '第${index}',
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  Divider(),
+                ],
+              );
+            },
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            height: double.infinity,
+            color: Colors.blue,
+            child: const Text('右侧'),
+          ),
+        ),
+      ],
+    );
   }
 }
