0fb91b27ff69fb7cfc287e2fd3ba811acc6d796c
zengchanghuan <zengchanghuan@gmail.com>
2022-05-17 19:01:05 +0800

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'routers/router.dart';
-
+import 'provider/counter.dart';
+import 'package:provider/provider.dart';
+import './pages/tabs/Cart.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -15,16 +17,20 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: const Size(750, 1624),    //配置设计稿的宽度高度
-        builder: () => MaterialApp(
-            debugShowCheckedModeBanner: false,
-            initialRoute: '/',
-            onGenerateRoute: onGenerateRoute,
-            theme: ThemeData(   
-              // primaryColor: Colors.yellow           
-              primaryColor: Colors.white
-            ),
-        )
-    );
+        designSize: const Size(750, 1624), //配置设计稿的宽度高度
+        builder: () => MultiProvider(
+              providers: 
+              [
+                ChangeNotifierProvider(create: (_) => Counter()),
+              ],
+              child: MaterialApp(
+                debugShowCheckedModeBanner: false,
+                initialRoute: '/',
+                onGenerateRoute: onGenerateRoute,
+                theme: ThemeData(
+                    // primaryColor: Colors.yellow
+                    primaryColor: Colors.white),
+              ),
+            ));
   }
 }
--- lib/pages/tabs/cart.dart
+++ lib/pages/tabs/cart.dart
@@ -1,20 +1,26 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../provider/Counter.dart';
 
 class CartPage extends StatefulWidget {
-  CartPage({Key? key}) : super(key: key);
+  const CartPage({Key? key}) : super(key: key);
 
+  @override
   _CartPageState createState() => _CartPageState();
 }
 
 class _CartPageState extends State<CartPage> {
+  @override
+  void initState() {
+    super.initState();
+    if (kDebugMode) {
+      print("cart");
+    }
+  }
 
   @override
-  void initState() { 
-    super.initState();
-    print("Cart init");
-  }
-  @override
   Widget build(BuildContext context) {
-    return Text("购物车页面");
+    return Text('234543');
   }
 }
