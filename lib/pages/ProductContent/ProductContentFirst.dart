000928cd259bcbf4fc3eb6be16fffc4d60c2377f
zengchanghuan <zengchanghuan@gmail.com>
2022-05-18 21:04:40 +0800

    消除警告

--- lib/pages/ProductContent/ProductContentFirst.dart
+++ lib/pages/ProductContent/ProductContentFirst.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import '../../services/ScreenAdapter.dart';
 import '../../widget/JdButton.dart';
@@ -9,8 +10,9 @@ import '../../config/Config.dart';
 
 class ProductContentFirst extends StatefulWidget {
   final List _productContentList;
-  ProductContentFirst(this._productContentList, {Key? key}) : super(key: key);
+  const ProductContentFirst(this._productContentList, {Key? key}) : super(key: key);
 
+  @override
   _ProductContentFirstState createState() => _ProductContentFirstState();
 }
 
@@ -23,14 +25,15 @@ class _ProductContentFirstState extends State<ProductContentFirst>
 
   String _selectedValue="";
 
+  @override
   bool get wantKeepAlive => true;
 
   @override
   void initState() {
     super.initState();
-    this._productContent = widget._productContentList[0];
+    _productContent = widget._productContentList[0];
 
-    this._attr = this._productContent.attr;
+    _attr = _productContent.attr;
 
     _initAttr();
 
@@ -76,7 +79,7 @@ class _ProductContentFirstState extends State<ProductContentFirst>
   //初始化Attr 格式化数据
   _initAttr() {
     //注意attrList属性需要在model中定义
-    var attr = this._attr;
+    var attr = _attr;
     for (var i = 0; i < attr.length; i++) {
       for (var j = 0; j < attr[i].list.length; j++) {
         if (j == 0) {
@@ -86,15 +89,18 @@ class _ProductContentFirstState extends State<ProductContentFirst>
         }
       }
     }
-    // print(attr[0].attrList);
-    // print(attr[0].cate);
-    // print(attr[0].list);
+    if (kDebugMode) {
+      print(attr[0].attrList);
+      print(attr[0].cate);
+      print(attr[0].list);
+    }
+
     _getSelectedAttrValue();
   }
 
   //改变属性值
   _changeAttr(cate, title, setBottomState) {
-    var attr = this._attr;
+    var attr = _attr;
     for (var i = 0; i < attr.length; i++) {
       if (attr[i].cate == cate) {
         for (var j = 0; j < attr[i].attrList.length; j++) {
@@ -107,13 +113,13 @@ class _ProductContentFirstState extends State<ProductContentFirst>
     }
     setBottomState(() {
       //注意  改变showModalBottomSheet里面的数据 来源于StatefulBuilder
-      this._attr = attr;
+      _attr = attr;
     });
     _getSelectedAttrValue();
   }
   //获取选中的值
   _getSelectedAttrValue() {
-    var _list = this._attr;
+    var _list = _attr;
     List tempArr = [];
     for (var i = 0; i < _list.length; i++) {
       for (var j = 0; j < _list[i].attrList.length; j++) {
@@ -124,7 +130,7 @@ class _ProductContentFirstState extends State<ProductContentFirst>
     }
     // print(tempArr.join(','));
     setState(() {
-      this._selectedValue = tempArr.join(',');
+      _selectedValue = tempArr.join(',');
     });
   }
 
@@ -133,14 +139,14 @@ class _ProductContentFirstState extends State<ProductContentFirst>
     List<Widget> attrItemList = [];
     attrItem.attrList.forEach((item) {
       attrItemList.add(Container(
-        margin: EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
         child: InkWell(
           onTap: () {
             _changeAttr(attrItem.cate, item["title"], setBottomState);
           },
           child: Chip(
             label: Text("${item["title"]}"),
-            padding: EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
             backgroundColor: item["checked"] ? Colors.red : Colors.black26,
           ),
         ),
@@ -152,18 +158,18 @@ class _ProductContentFirstState extends State<ProductContentFirst>
   //封装一个组件 渲染attr
   List<Widget> _getAttrWidget(setBottomState) {
     List<Widget> attrList = [];
-    this._attr.forEach((attrItem) {
+    _attr.forEach((attrItem) {
       attrList.add(Wrap(
         children: <Widget>[
-          Container(
+          SizedBox(
             width: ScreenAdapter.width(120),
             child: Padding(
               padding: EdgeInsets.only(top: ScreenAdapter.height(22)),
               child: Text("${attrItem.cate}: ",
-                  style: TextStyle(fontWeight: FontWeight.bold)),
+                  style: const TextStyle(fontWeight: FontWeight.bold)),
             ),
           ),
-          Container(
+          SizedBox(
             width: ScreenAdapter.width(590),
             child: Wrap(
               children: _getAttrItemWidget(attrItem, setBottomState),
@@ -204,9 +210,9 @@ class _ProductContentFirstState extends State<ProductContentFirst>
                           Expanded(
                             flex: 1,
                             child: Container(
-                              margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                              margin: const EdgeInsets.fromLTRB(10, 0, 0, 0),
                               child: JdButton(
-                                color: Color.fromRGBO(253, 1, 0, 0.9),
+                                color: const Color.fromRGBO(253, 1, 0, 0.9),
                                 text: "加入购物车",
                                 cb: () {
                                   print('加入购物车');
@@ -217,12 +223,14 @@ class _ProductContentFirstState extends State<ProductContentFirst>
                           Expanded(
                             flex: 1,
                             child: Container(
-                                margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                margin:const EdgeInsets.fromLTRB(10, 0, 10, 0),
                                 child: JdButton(
-                                  color: Color.fromRGBO(255, 165, 0, 0.9),
+                                  color:const Color.fromRGBO(255, 165, 0, 0.9),
                                   text: "立即购买",
                                   cb: () {
-                                    print('立即购买');
+                                    if (kDebugMode) {
+                                      print('立即购买');
+                                    }
                                   },
                                 )),
                           )
@@ -239,11 +247,12 @@ class _ProductContentFirstState extends State<ProductContentFirst>
   @override
   Widget build(BuildContext context) {
     //处理图片
+    super.build(context);
     String pic = Config.domain + this._productContent.pic;
     pic = pic.replaceAll('\\', '/');
 
     return Container(
-      padding: EdgeInsets.all(10),
+      padding:const EdgeInsets.all(10),
       child: ListView(
         children: <Widget>[
           AspectRatio(
@@ -252,28 +261,28 @@ class _ProductContentFirstState extends State<ProductContentFirst>
           ),
           //标题
           Container(
-            padding: EdgeInsets.only(top: 10),
-            child: Text("${this._productContent.title}",
+            padding:const EdgeInsets.only(top: 10),
+            child: Text("${_productContent.title}",
                 style: TextStyle(
                     color: Colors.black87, fontSize: ScreenAdapter.size(36))),
           ),
           Container(
-              padding: EdgeInsets.only(top: 10),
-              child: Text("${this._productContent.subTitle}",
+              padding:const EdgeInsets.only(top: 10),
+              child: Text("${_productContent.subTitle}",
                   style: TextStyle(
                       color: Colors.black54,
                       fontSize: ScreenAdapter.size(28)))),
           //价格
           Container(
-            padding: EdgeInsets.only(top: 10),
+            padding: const EdgeInsets.only(top: 10),
             child: Row(
               children: <Widget>[
                 Expanded(
                   flex: 1,
                   child: Row(
                     children: <Widget>[
-                      Text("特价: "),
-                      Text("¥${this._productContent.price}",
+                      const Text("特价: "),
+                      Text("¥${_productContent.price}",
                           style: TextStyle(
                               color: Colors.red,
                               fontSize: ScreenAdapter.size(46))),
@@ -285,8 +294,8 @@ class _ProductContentFirstState extends State<ProductContentFirst>
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: <Widget>[
-                      Text("原价: "),
-                      Text("¥${this._productContent.oldPrice}",
+                      const Text("原价: "),
+                      Text("¥${_productContent.oldPrice}",
                           style: TextStyle(
                               color: Colors.black38,
                               fontSize: ScreenAdapter.size(28),
@@ -298,9 +307,9 @@ class _ProductContentFirstState extends State<ProductContentFirst>
             ),
           ),
           //筛选
-          this._attr.length > 0
+          _attr.isNotEmpty
               ? Container(
-                  margin: EdgeInsets.only(top: 10),
+                  margin:const EdgeInsets.only(top: 10),
                   height: ScreenAdapter.height(80),
                   child: InkWell(
                     onTap: () {
@@ -308,25 +317,25 @@ class _ProductContentFirstState extends State<ProductContentFirst>
                     },
                     child: Row(
                       children: <Widget>[
-                        Text("已选: ",
+                        const Text("已选: ",
                             style: TextStyle(fontWeight: FontWeight.bold)),
-                        Text("${this._selectedValue}")
+                        Text("${_selectedValue}")
                       ],
                     ),
                   ),
                 )
-              : Text(""),
-          Divider(),
-          Container(
+              : const Text(""),
+          const Divider(),
+          SizedBox(
             height: ScreenAdapter.height(80),
             child: Row(
-              children: <Widget>[
+              children: const <Widget>[
                 Text("运费: ", style: TextStyle(fontWeight: FontWeight.bold)),
                 Text("免运费")
               ],
             ),
           ),
-          Divider(),
+          const Divider(),
         ],
       ),
     );
