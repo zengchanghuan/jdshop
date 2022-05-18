bf28f6a119ad4555e66e3e2bb890ef4c86a054a4
zengchanghuan <zengchanghuan@gmail.com>
2022-05-18 19:09:28 +0800

    加载webview

--- lib/pages/ProductContent/ProductContentSecond.dart
+++ lib/pages/ProductContent/ProductContentSecond.dart
@@ -1,17 +1,49 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import '../../widget/LoadingWidget.dart';
 
 class ProductContentSecond extends StatefulWidget {
   final List _productContentList;
 
-  const ProductContentSecond(this._productContentList,{Key? key}) : super(key: key);
+  const ProductContentSecond(this._productContentList, {Key? key})
+      : super(key: key);
 
   @override
   _ProductContentSecondState createState() => _ProductContentSecondState();
 }
 
-class _ProductContentSecondState extends State<ProductContentSecond> {
+class _ProductContentSecondState extends State<ProductContentSecond>
+    with AutomaticKeepAliveClientMixin {
+  dynamic _id;
+
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  void initState() {
+    super.initState();
+    _id = widget._productContentList[0].sId;
+    if (kDebugMode) {
+      print(_id);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text("详情");
+    super.build(context);
+    return Column(
+      children: [
+        Expanded(
+            child: WebView(
+          initialUrl: "https://jdmall.itying.com/pcontent?id=$_id",
+        ))
+        // Expanded(
+        //     child: WebView(
+        //       initialUrl:"https://jdmall.itying.com/pcontent?id=5a0425bc010e711234661439",
+        //
+        //     ))
+      ],
+    );
   }
 }
